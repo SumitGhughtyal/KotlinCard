fffcd9124da1a31912e3fbd8795a424159d28043
SumitGhughtyal <sumitghughtyal@gmail.com>
2024-01-06 04:41:44 +0530

    1 commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,96 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.surfaceColorAtElevation
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.myapplication.ui.theme.MyApplicationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            val painter= painterResource(id = R.drawable.ted)
+            val description="Ted meets Tracy"
+            val title="And this is How I Met Your Mother"
+
+            Box(modifier = Modifier.fillMaxSize(0.5f).padding(16.dp)) {
+                ImageCard(painter = painter, contentDescription = description, title = title)
+            }
+
+        }
+    }
+}
+
+@Composable
+fun ImageCard(
+    painter: Painter,
+    contentDescription: String,
+    title: String,
+    modifier: Modifier=Modifier
+    ){
+    Card(
+        modifier=modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(15.dp),
+        elevation = CardDefaults.cardElevation(5.dp)
+    ) {
+        Box (modifier = Modifier.height(200.dp)){
+            Image(
+                painter = painter,
+                contentDescription =contentDescription,
+                contentScale = ContentScale.Crop
+            )
+
+            Box(modifier=Modifier.fillMaxSize()
+                .background(
+                    Brush.verticalGradient(colors = listOf(
+                        Color.Transparent,
+                        Color.Black,
+                    )
+                        ,startY = 300f,
+                        )
+                ))
+
+
+            Box(modifier= Modifier
+                .fillMaxSize()
+                .padding(12.dp),
+            contentAlignment = Alignment.BottomStart)
+            {
+                Text(title, style= TextStyle(color = Color.White, fontSize=16.sp))
+
+            }
+        }
+
+    }
+
+}
